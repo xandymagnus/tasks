d73dcb802993f67b567ba8060778d56ad9ad092b
xandymagnus <xandy.magno325@gmail.com>
2026-02-25 10:20:04 -0300

    fix: correct event handling and improve form validation

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const form = document.getElementById("form")
-const inputEmail = document.getElementById("email")
-const inputSenha = document.getElementById("senha")
+const form = document.getElementById("form");
+const inputEmail = document.getElementById("email");
+const inputSenha = document.getElementById("senha");
 
 form.addEventListener("submit", function(event){
     
-    event.defaultPrevented()
+    event.defaultPrevented();
 
-    const email = inputEmail.value
-    const senha = inputSenha.value
+    const email = inputEmail.value;
+    const senha = inputSenha.value;
 
-    
+    if (email === "" || senha === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    window.location.href = "task.html";
 })
