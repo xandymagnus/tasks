fd6049ec2387ebfffedcf2516c3e2e7549885cd7
xandymagnus <xandy.magno325@gmail.com>
2026-08-27 10:52:19 -0300

    fix: validate confirmation password in form submission

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form");
 const inputEmail = document.getElementById("email");
 const inputSenha = document.getElementById("senha");
+const inputConfirmaSenha = document.getElementById("confirmaSenha");
 
 form.addEventListener("submit", function(event) {
     
@@ -8,11 +9,26 @@ form.addEventListener("submit", function(event) {
 
     const email = inputEmail.value;
     const senha = inputSenha.value;
+    const confirmaSenha = inputConfirmaSenha.value;
 
-    if (email === "" || senha === "") {
+    if (email === "" || senha === "" || confirmaSenha === "") {
         alert("Preencha todos os campos!");
         return;
     };
+    
+    if (senha != confirmaSenha) {
+        alert("As senhas precisam ser iguais!")
+        limpaCampo()
+        return;
+    } else {
+        alert("Senhas iguais")
+    }
 
     window.location.href = "/html/tasks.html";
 });
+
+function limpaCampo () {
+    inputEmail.value = ""
+    inputSenha.value = ""
+    inputConfirmaSenha.value = ""
+}
